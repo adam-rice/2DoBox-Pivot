9bc7d9ec3ed86a7aa7c17f6ff49b568a71d0d728
Chaz-Mykl <chaz.mykl@gmail.com>
2016-10-24 14:29:08 -0600

    New idea prepends.

--- function.js
+++ function.js
@@ -4,7 +4,22 @@ var saveButton = $('#saveButton');
 var bottomPanel = $('#bottomPanel');
 var ideaSection = $('#ideaSection');
 
-
+saveButton.on('click', function () {
+  ideaSection.prepend(`
+    <li class="idea-card">
+      <header>
+        <h3>${titleInput.val()}</h3>
+        <button class="remove-button"><img src="delete-hover.svg"/></button>
+      </header>
+      <p class="body">${bodyInput.val()}</p>
+      <footer>
+        <button class="upvote"><img src="upvote-hover.svg"/></button>
+        <button class="downvote"><img src="downvote-hover.svg"/></button>
+        <p class="quality">quality:<span>swill</span></p>
+      </footer>
+    </li>
+    `);
+});
 
 
 
