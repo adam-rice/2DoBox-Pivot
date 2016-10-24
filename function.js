2dc96f7332334a741a05990e65fa75fa54d719cf
Chaz-Mykl <chaz.mykl@gmail.com>
2016-10-24 13:50:53 -0600

    Constructor Function.

--- function.js
+++ function.js
@@ -3,3 +3,20 @@ var bodyInput = $('#bodyInput');
 var saveButton = $('#saveButton');
 var bottomPanel = $('#bottomPanel');
 var ideaSection = $('#ideaSection');
+
+
+
+
+
+
+
+
+
+
+
+function Idea(title, body, quality, id) {
+  this.title = title;
+  this.body = body;
+  this.quality = quality || "swill";
+  this.id = id || Date.now();
+}
