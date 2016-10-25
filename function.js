df9deb7561b3b82552e93c46ec6d9de1f33cf6c6
Adam Rice <adamcoltonrice@gmail.com>
2016-10-25 13:29:45 -0600

    WIP editing title function

--- function.js
+++ function.js
@@ -132,7 +132,16 @@ $(document).ready (function() {
   });
 
   //listener on title and bodyInput
-  
+  ideaSection.on('keydown click', 'h3, p', function(key) {
+    var id = $(this).closest('.idea-card');
+    // addclass
+    if (key.which === 13) {
+      if (event.target.nodeName === 'H3') {
+        var newTitle = $(this).closest('h3').text();
+        ideaBoss.find(id);
+      }
+    }
+  });
 
   //listener on search for title
 
