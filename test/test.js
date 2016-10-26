eb33f7eb91e22ec11724d0781fcbdbe8fd564ab5
Adam Rice <adamcoltonrice@gmail.com>
2016-10-26 12:04:13 -0600

    testing files updated

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var Idea = require('../exercises/idea');
+
+describe('Idea', function () {
+
+  it('should be a function', function () {
+    assert.isFunction(Idea);
+  });
+
+});
